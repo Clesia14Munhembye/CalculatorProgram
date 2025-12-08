7416bc38bd9c76b88731211f8bbdbe9a7f00f287
Cleu <munhembyesergio94clesia@gmail.com>
2025-12-07 21:37:57 -0800

    my Calculator

---
+++ Calculator.py
@@ -0,0 +1,16 @@
+print ("Wellcame to my calculator")
+
+
+num1 = float(input ("enter the first number:"))
+
+num2 = float(input ("enter the second number:"))
+
+addition = num1 + num2
+subtractions = num1 - num2
+multiplication= num1 * num2
+division = num1 / num2
+
+print("Addition:", addition)
+print("A subtraction:", subtractions)
+print("multiplication:", multiplication)
+print ("division:", division) 
