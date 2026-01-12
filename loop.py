5b6c1e082d79014a309d34e304f4a45bba269d41
Cleu <munhembyesergio94clesia@gmail.com>
2026-01-12 00:52:17 -0800

    calculate_discount

---
+++ loop.py
@@ -0,0 +1,14 @@
+#loop conditionals
+fruits =["apple", "banana", "cherry"]
+for fruit in fruits:
+    print (f" i love {fruit}")
+    
+    #While loop
+    
+    count = 2
+    
+    while count <= 5:
+        print (f"Count: {count}")
+        count += 2
+        
+    
