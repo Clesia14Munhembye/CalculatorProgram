907ec91fd38d80020d416535197cc9f385af0b2d
Cleu <munhembyesergio94clesia@gmail.com>
2026-01-31 01:11:53 -0800

    Done, structure of Development

---
+++ polymorphism.py
@@ -0,0 +1,22 @@
+class Dog:
+    def speak(self):
+        return "Woof!"
+
+class Cat:
+    def speak(self):
+        return "Meow!"
+
+# Polymorphism in action
+for animal in [Dog(), Cat()]:
+    print(animal.speak())
+    
+    #encapsulament   class SecretStash:
+    def __init__(self):
+        self.__chocolates = 10  # Private attribute
+
+    def take_chocolate(self):
+        if self.__chocolates > 0:
+            self.__chocolates -= 1
+            print("One chocolate taken!")
+        else:
+            print("No chocolates left 😢")
